5069eb14841c464d4f1939c6d77147ca2efedf01
madhusudanlolge <iammadhu4299@gmail.com>
2022-05-02 09:54:36 +0530

    do

--- Jenkins.java
+++ Jenkins.java
@@ -10,6 +10,12 @@ public class Jenkins {
        
 	for (int i = 0; i < arr.length; i++) {  
 	System.out.print(arr[i] + " ");  
+
+	//changes in the code to reflect it on jenkins via git
+	System.out.print("This is ASDM Assignment Number 10");  
+	System.out.print("Changes Done in Local Repo has to reflect on jenkins ");  
+	System.out.print("Changes done");  
+
         }  
     }  
 }  
